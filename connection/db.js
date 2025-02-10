285d10f094b83d90851158752221f578b96cd0e7
muhammed yasin <99520470+yasinputhiyara@users.noreply.github.com>
2025-02-10 10:53:16 +0530

    Update db.js

--- connection/db.js
+++ connection/db.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 const connect = mongoose.connect(
-  "mongodb://127.0.0.1:27017/ecommerce"
+  // "mongodb://127.0.0.1:27017/ecommerce"
+  // "mongodb+srv://yasinputhiyara:Yasin@123@cluster0.nfpnz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  "mongodb+srv://yasinputhiyara:Yasin%40123@cluster0.nfpnz.mongodb.net/ecommerce?retryWrites=true&w=majority&appName=Cluster0"
   // { useNewUrlParser: true, useUnifiedTopology: true}
 );
 
